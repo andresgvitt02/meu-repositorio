abb5c592a2e9868314e82c387eb241dc3810f315
andresgvitt02 <andresgit@outlook.com>
2024-12-02 22:01:44 -0300

    AS

---
+++ 4semestre/Avaliacao semestral/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        RegrasJogo jogo = new RegrasJogo();
+        jogo.configurarJogo( 550000.0, 9000.0, 10);
+        jogo.iniciarJogo();
+    }
+}
